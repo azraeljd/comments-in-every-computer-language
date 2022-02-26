5fa2b273c6efb99948fe88d8845f6d02e205ec11
Azrael JD <94840719+azraeljd@users.noreply.github.com>
2022-02-26 14:01:43 +0700

    Add Python comments

---
+++ p/python.py
@@ -0,0 +1,13 @@
+print("Hello, World!")    # This is an inline comment
+
+"""
+This is a multi line comment,
+but this comment will appear in the program's __doc__ string.
+"""
+
+# This
+# is
+# a
+# multi
+# line
+# comment
