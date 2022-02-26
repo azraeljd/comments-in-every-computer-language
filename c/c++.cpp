d2a28abcd5241b6923e1531dc90e4faabb0f7c24
Azrael JD <94840719+azraeljd@users.noreply.github.com>
2022-02-26 14:05:31 +0700

    Add C++ comments

---
+++ c/c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+   std::cout << "Hello World" << std::endl;    // This is an inline comment
+}
+
+/*
+    This
+    is
+    a
+    multi
+    line
+    comment
+*/
