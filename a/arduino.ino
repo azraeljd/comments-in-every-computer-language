7ef381a63203caef1a0f7bac504212b3da2f7d0d
Azrael JD <94840719+azraeljd@users.noreply.github.com>
2022-03-02 22:00:34 +0700

    Add Arduino comments

---
+++ a/arduino.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello, World!");    // This is an inline comment
+}
+
+void loop() {
+
+}
+
+/*
+    This
+    is
+    a
+    multi
+    line
+    comment
+*/
